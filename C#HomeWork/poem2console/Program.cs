b6b804427bb22ce5cda74fa520589c521f3ec1f7
LeTuanAnh <leanh250103@gmail.com>
2024-12-26 00:48:44 +0700

    Revert "thuc hanh tao solution"
    This reverts commit 372048e8ae9054304c9102270e11517939caba4a.

--- C#HomeWork/poem2console/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.WriteLine("Anh về quê cũ: thôn Vân");
-Console.WriteLine("Sau khi đã biết phong trần ra sao.");
-Console.WriteLine("Từ nay lại tắm ao đào");
-Console.WriteLine("Rượu dâu nhà cất, thuốc lào nhà phơi.");
--- C#HomeWork/poem3console/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.WriteLine("Tình cờ không hẹn bỗng mà nên");
-Console.WriteLine("Một buổi đầu năm tết Kỷ Hợi");
-Console.WriteLine("Có năm người bạn bến sông Hoàng");
-Console.WriteLine("Gặp nhau uống rượu mừng năm mới");
