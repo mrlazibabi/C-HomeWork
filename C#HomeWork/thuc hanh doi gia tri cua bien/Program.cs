e0bee27244b9ceb1d1790e5ae7d7b929e639af57
LeTuanAnh <leanh250103@gmail.com>
2024-12-26 22:42:07 +0700

    day2-bai4: thực hành đổi giá trị của biến

---
+++ C#HomeWork/thuc hanh doi gia tri cua bien/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Hãy nhập giá trị đầu tiên: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hãy nhập giá trị thứ hai: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nĐang thực hiện đổi thứ tự giữa 2 giá trị.....");
+int temp;
+temp = value1;
+value1 = value2;
+value2 = temp;
+
+Console.WriteLine("\nĐây là sau khi đã đổi thứ tự giữa 2 giá trị: ");
+Console.WriteLine("Giá trị value1 sau khi đổi: " + value1);
+Console.WriteLine("Giá trị value2 sau khi đổi: " + value2);
