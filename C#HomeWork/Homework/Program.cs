d1a69a24f3137f82426ec0ce084622d9c4920de8
LeTuanAnh <leanh250103@gmail.com>
2024-12-26 22:33:34 +0700

    day2-bai3: thực hành nhập và chuyển đổi dữ liệu

--- C#HomeWork/Homework/Program.cs
+++ C#HomeWork/Homework/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Nhập năm sinh: ");
+int birthday = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhập chiều cao (cm): ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Nhập cân nặng (kg): ");
+double weight = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Nhập điểm trung bình: ");
+double averagePoint = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Nhập trạng thái hôn nhân: ");
+string marriage = Console.ReadLine();
+
+/////////////////////////////////////
+Console.WriteLine("\n Thông tin đã nhập:");
+Console.WriteLine("Năm sinh: "+birthday);
+Console.WriteLine("Chiều cao: "+height);
+Console.WriteLine("Cân nặng: "+weight);
+Console.WriteLine("Điểm trung bình: "+averagePoint);
+Console.WriteLine("Trạng thái hôn nhân: "+marriage);
