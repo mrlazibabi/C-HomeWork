73f1a1136d3ff432a4ed6a3ec405b1a9c131048e
LeTuanAnh <leanh250103@gmail.com>
2024-12-28 22:27:07 +0700

    day3-bai1: toan tu so sanh

---
+++ C#HomeWork/toan su so sanh/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, x, y, m, n, p, q, c, d, i, j, e, f, r, s, k, l, u, v;
+
+Console.WriteLine($"1. a = {a=7}, b = {b=3} => a > b : " + (a>b));
+Console.WriteLine($"2. x = {x = 10}, y = {y = 15} => x <= y : " + (x <= y));
+Console.WriteLine($"3. m = {m = 5}, n = {n = 5} => m == n : " + (m == n));
+Console.WriteLine($"4. p = {p = 8}, q = {q = 8} => p != q : " + (p != q));
+Console.WriteLine($"5. c = {c = 12}, d = {d = 9} => c >= d : " + (c >= d));
+Console.WriteLine($"6. i = {i = 4}, j = {j = 2} => i < j : " + (i < j));
+Console.WriteLine($"7. e = {e = 6}, f = {f = 6} => e <= f : " + (e <= f));
+Console.WriteLine($"8. r = {r = 9}, s = {s = 9} => r != s : " + (r != s));
+Console.WriteLine($"9. k = {k = 14}, l = {l = 20} => k > l : " + (k > l));
+Console.WriteLine($"10. u = {u = 3}, v = {v = 3} => u >= v : " + (u >= v));
