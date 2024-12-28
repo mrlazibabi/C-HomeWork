259b1c4f76f9b12138c37549eafc2a0c5b01452d
LeTuanAnh <leanh250103@gmail.com>
2024-12-28 21:54:36 +0700

    day3-bai2: tinh chu vi hinh chu nhat

---
+++ C#HomeWork/tinh chu vi hinh chu nhat/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Nhập chiều dài hình chữ nhật: ");
+double length = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Nhập chiều rộng hình chữ nhật: ");
+double width = Convert.ToDouble(Console.ReadLine());
+
+double p = 2 * (width + length);
+
+Console.WriteLine($"Ta có chu vi hình chữ nhật là : 2 * ( {width} + {length}) = {p}");
