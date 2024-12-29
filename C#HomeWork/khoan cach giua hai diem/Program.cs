6312c64c36ac03a69c2bb084594cd4ef2b7e3139
LeTuanAnh <leanh250103@gmail.com>
2024-12-29 22:14:45 +0700

    day3-bai3: tinh khoang cach giua hai diem

---
+++ C#HomeWork/khoan cach giua hai diem/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Math;
+
+Console.WriteLine("Nhap toa do diem x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhap toa do diem y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhap toa do diem x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhap toa do diem y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double khoangCach = Math.Sqrt((Math.Pow((x2-x1),2)) + (Math.Pow((y2 - y1),2)));
+
+Console.WriteLine("Khoang cach giua hai diem la: " + khoangCach);
