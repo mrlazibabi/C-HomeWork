4cab9affe4197b72186308f614dc1f7b825d76f4
LeTuanAnh <leanh250103@gmail.com>
2024-12-29 22:53:58 +0700

    day3-bai6: thu thap quang

---
+++ C#HomeWork/thu thap quang/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hay nhap so quang da dao duoc: ");   
+int totalOres = Convert.ToInt32(Console.ReadLine());
+int coins = 0;
+
+int ores = Math.Min(totalOres, 10);
+coins += ores*10;       
+totalOres -= ores;
+
+ores = Math.Min(totalOres, 5);
+coins += ores*5;      
+totalOres -= ores;               
+
+ores = Math.Min(totalOres, 3);
+coins += ores*2;     
+totalOres -= ores;
+
+coins += totalOres*1;         
+
+Console.WriteLine("Tong so xu vang thu duoc: " + coins);
