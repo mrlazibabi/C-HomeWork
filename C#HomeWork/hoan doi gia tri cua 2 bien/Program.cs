e03a9c86348bc43e1055f4fb51a10b4d6fd0665c
LeTuanAnh <leanh250103@gmail.com>
2024-12-29 22:36:59 +0700

    day3-bai5: hoan doi gia tri 2 bien

---
+++ C#HomeWork/hoan doi gia tri cua 2 bien/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Nhap gia tri cua a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhap gia tri cua b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int sum = a + b;
+
+Console.WriteLine("Gia tri cua a truoc khi doi la: "+a);
+Console.WriteLine("Gia tri cua b truoc khi doi la: "+b);
+
+Console.WriteLine("\nGia tri cua a sau khi doi la: "+(sum-a));
+Console.WriteLine("Gia tri cua b sau khi doi la: "+(sum-b));
+
+
+
