ebea2f8eeb5e8bb2c7c6f65b0749bc3fd33fe579
LeTuanAnh <leanh250103@gmail.com>
2024-12-30 23:06:18 +0700

    day4-bai5: tim so chan le (switch case)

---
+++ C#HomeWork/kiem tra chan le switch case/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hay nhap 1 so nguyen tu 1-100: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 0 && number <= 100)
+{
+    switch (number % 2)
+    {
+        case 0:
+            Console.WriteLine($"{number} la so chan va nam trong day so 1-100");
+            break;
+        default:
+            Console.WriteLine($"{number} la so le va nam trong day so 1-100");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine($"{number} khong nam trong day so 1-100");
+}
