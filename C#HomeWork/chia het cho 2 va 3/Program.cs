3928e357a45489920077911718b5deae3e475e12
LeTuanAnh <leanh250103@gmail.com>
2025-01-01 19:59:10 +0700

    day4-bai7: chia het cho 2 va 3

---
+++ C#HomeWork/chia het cho 2 va 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hay nhap 1 so nguyen:");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+switch(num % 6)
+{
+    case 0:
+        Console.WriteLine($"So nguyen {num} chia het cho ca 2 va 3");
+        break;
+    default:
+        Console.WriteLine($"So nguyen {num} khong chia het cho ca 2 va 3");
+        break;
+}
