8b7b0002cfa228d926a4334b2e5683e9fffa2d8a
LeTuanAnh <leanh250103@gmail.com>
2025-01-01 16:37:56 +0700

    day4-bai6: chuong trinh may tinh

---
+++ C#HomeWork/chuong trinh maty tinh don gian/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hay nhap so thu nhat: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Hay nhap so thu hai: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Hay chon mot trong cac phep toan sau (+, -, *, /): ");
+string mathOperator = Console.ReadLine();
+
+switch (mathOperator)
+{
+    case "+":
+        Console.WriteLine($"Ket qua: {num1 + num2}");
+        break;
+    case "-":
+        Console.WriteLine($"Ket qua: {num1 - num2}");
+        break;
+    case "*":
+        Console.WriteLine($"Ket qua: {num1 * num2}");
+        break;
+    case "/":
+        Console.WriteLine($"Ket qua: {num1 + num2}");
+        break;
+    default:
+        Console.WriteLine("Khong co phep tinh nay!");
+        break;
+}
