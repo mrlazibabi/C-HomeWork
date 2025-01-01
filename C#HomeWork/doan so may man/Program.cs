6a10056782145d6e37db84c9cb5d50f65d020f32
LeTuanAnh <leanh250103@gmail.com>
2025-01-01 20:05:23 +0700

    day4-bai8: doan so may man

---
+++ C#HomeWork/doan so may man/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hay nhap 1 so nguyen bat ki trong khoang 1-10");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"So ban chon la: "+num);
+
+int random = Random.Shared.Next(1, 11);
+Console.WriteLine("So may man la: "+random);
+
+if (num == random)
+{
+    Console.WriteLine("Ban that la may man!!! Hay mau chong di mua ve so.");
+}
+else
+{
+    Console.WriteLine("Try again next time.");
+}
+
