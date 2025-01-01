f6cd892e28b7812d8e032568f5a32d53d16fdbf7
LeTuanAnh <leanh250103@gmail.com>
2025-01-01 23:04:52 +0700

    day5-bai6: tinh tong cac chu so

---
+++ C#HomeWork/tinh tong cac chu so/Program.cs
@@ -0,0 +1,22 @@
+﻿int number;
+
+do
+{
+    Console.Write("Hay nhap mot so nguyen duong: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    int sum = 0;
+    int temp = number;
+
+    while (temp > 0)
+    {
+        //lay chu so cuoi cung roi cong vao sum
+        sum += temp % 10; 
+
+        //bo chu so cuoi cung
+        temp /= 10;       
+    }
+
+    Console.WriteLine($"Tong cac chu so cua {number} la: {sum}");
+    break; 
+} while (true);
