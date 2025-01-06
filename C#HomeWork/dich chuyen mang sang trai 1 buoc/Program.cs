c8c999a048652b48ce110dede50d94d8aad935b5
LeTuanAnh <leanh250103@gmail.com>
2025-01-06 16:18:55 +0700

    day6-bai3: dich cac phan tu trong mang sang trai 1 buoc

---
+++ C#HomeWork/dich chuyen mang sang trai 1 buoc/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6 };
+
+Console.WriteLine("Ta co 1 mang la array = { 1, 2, 3, 4, 5, 6 }");
+Console.Write("Noi dung cua mang: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+int firstE = array[0];
+for(int i = 0; i < array.Length - 1; i++)
+{
+    array[i] = array[i+1];
+}
+array[array.Length-1] = firstE;
+
+Console.Write("Noi dung cua mang sau khi duoc dich sang trai 1 buoc: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
