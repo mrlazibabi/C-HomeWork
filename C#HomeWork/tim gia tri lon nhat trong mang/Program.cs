16c2de28678d6c2a49b754f5afce33b287a32bb7
LeTuanAnh <leanh250103@gmail.com>
2025-01-06 15:33:38 +0700

    day6-bai1: tim gia tri lon nhat trong mang

---
+++ C#HomeWork/tim gia tri lon nhat trong mang/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 8, 4, 5, 9, 6, 1, 2, 7, 3 };
+int maxNum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > maxNum)
+    {
+        maxNum = array[i];
+    }
+}
+
+Console.WriteLine("Ta co 1 mang la array = { 8, 4, 5, 9, 6, 1, 2, 7, 3 }");
+Console.WriteLine($"Gia tri lon nhat trong mang la {maxNum}");
