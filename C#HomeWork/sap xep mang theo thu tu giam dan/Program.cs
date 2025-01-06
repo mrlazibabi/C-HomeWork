70f0cf0516670fba744c8b5a36f947a40d7faf91
LeTuanAnh <leanh250103@gmail.com>
2025-01-06 18:13:37 +0700

    day6-bai6: sap xep mang theo giam dan

---
+++ C#HomeWork/sap xep mang theo thu tu giam dan/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = { 5, 1, 9, 3, 7 };
+int temp = 0;
+
+Console.Write("Ta co mang la array = ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+for (int i = 0; i < array.Length-1; i++)
+{
+    for(int j = 0; j < array.Length - 1 - i; j++)
+    {
+        if (array[j] < array[j + 1])
+        {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    } 
+}
+Console.Write("\nTa co mang sau khi sap xep giam dan la array = ");
+for (int i = 0;i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
