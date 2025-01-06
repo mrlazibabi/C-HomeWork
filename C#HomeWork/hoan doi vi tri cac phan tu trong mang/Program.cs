54b1a7f88251c5972563f93c23fd9e6f4f96bd43
LeTuanAnh <leanh250103@gmail.com>
2025-01-06 15:54:48 +0700

    day6-bai2: hoan doi vi tri cac phan tu trong mang

---
+++ C#HomeWork/hoan doi vi tri cac phan tu trong mang/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6 };
+int[] temp = new int[6];
+
+for (int i = 0; i < array.Length; i++)
+{
+    temp[i] = array[array.Length - 1 - i];
+}
+
+Console.WriteLine("Ta co 1 mang la array = { 1, 2, 3, 4, 5, 6 }");
+Console.Write("Noi dung cua mang: ");
+for (int i = 0;i < array.Length; i++)
+{  
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+Console.Write("Noi dung cua mang sau khi duoc hoan doi: ");
+for (int i = 0; i < temp.Length; i++)
+{
+    Console.Write($"{temp[i]} ");
+}
+Console.WriteLine();
