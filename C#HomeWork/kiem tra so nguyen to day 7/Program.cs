695bd9c4d4f9cbdeb7856c1da910f390923ce145
LeTuanAnh <leanh250103@gmail.com>
2025-01-08 18:14:02 +0700

    day7-bai6: kiem tra so nguyen to

---
+++ C#HomeWork/kiem tra so nguyen to day 7/Program.cs
@@ -0,0 +1,24 @@
+﻿bool IsPrime(int number)
+{
+    bool isPrime = true;
+    if (number < 2)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return isPrime;
+}
+
+Console.WriteLine("Number 2 is a Prime number: " + IsPrime(2));
+Console.WriteLine("Number 7 is a Prime number: " + IsPrime(7));
+Console.WriteLine("Number 10 is a Prime number: " + IsPrime(10));
+Console.WriteLine("Number 17 is a Prime number: " + IsPrime(17));
