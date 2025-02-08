75289b5c88c4cf7d0d6c9208032a6abbdd0d5c49
LeTuanAnh <leanh250103@gmail.com>
2025-02-08 19:11:35 +0700

    day12-bai1: nang cap dao vang

--- C#HomeWork/thu thap quang/Program.cs
+++ C#HomeWork/thu thap quang/Program.cs
@@ -1,19 +1,50 @@
-﻿Console.WriteLine("Hay nhap so quang da dao duoc: ");   
-int totalOres = Convert.ToInt32(Console.ReadLine());
-int coins = 0;
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-int ores = Math.Min(totalOres, 10);
-coins += ores*10;       
-totalOres -= ores;
+try
+{
+    Console.WriteLine("Hãy nhập số quặng đã đào được: ");
+    int totalOres = int.Parse(Console.ReadLine());
 
-ores = Math.Min(totalOres, 5);
-coins += ores*5;      
-totalOres -= ores;               
+    MineGold mineGold = new MineGold();
+    int coins = mineGold.CalculatedGold(totalOres);
+    Console.WriteLine("Tổng số xu vàng thu được: " + coins);
+}
+catch(FormatException)
+{
+    Console.WriteLine("Số quặng đã nhập không hợp lệ.");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("Số quặng phải lớn hơn 0!");
+}
+catch(Exception ex) {  Console.WriteLine(ex.ToString()); }
 
-ores = Math.Min(totalOres, 3);
-coins += ores*2;     
-totalOres -= ores;
 
-coins += totalOres*1;         
+public class MineGold()
+{
+   public int CalculatedGold(int totalOres)
+    {
+        if (totalOres <= 0)
+        {
+            throw new ArgumentException();
+        }
 
-Console.WriteLine("Tong so xu vang thu duoc: " + coins);
+        int coins = 0;
+
+        int ores = Math.Min(totalOres, 10);
+        coins += ores * 10;
+        totalOres -= ores;
+
+        ores = Math.Min(totalOres, 5);
+        coins += ores * 5;
+        totalOres -= ores;
+
+        ores = Math.Min(totalOres, 3);
+        coins += ores * 2;
+        totalOres -= ores;
+
+        coins += totalOres * 1;
+
+        return coins;
+    }
+}
