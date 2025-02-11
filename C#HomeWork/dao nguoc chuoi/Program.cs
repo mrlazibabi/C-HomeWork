a9811129da4352399fd8ee35a7f704c679d04ac1
LeTuanAnh <leanh250103@gmail.com>
2025-02-11 21:35:04 +0700

    day13-bai1: dao nguoc chuoi

---
+++ C#HomeWork/dao nguoc chuoi/Program.cs
@@ -0,0 +1,40 @@
+﻿
+try
+{
+    StringReverser reverser = new StringReverser();
+
+    string result1 = reverser.Reverse("123456");
+    Console.WriteLine(result1); // 654321
+
+    string result2 = reverser.Reverse("OpenAI");
+    Console.WriteLine(result2); // IAnepO
+
+    string result5 = reverser.Reverse("");
+    Console.WriteLine(result5); // (chuỗi rỗng)
+
+    string result6 = reverser.Reverse(null); // ArgumentNullException 
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Message: {e.Message}");
+}
+
+
+class StringReverser
+{
+    public string Reverse(string input)
+    {
+        if(input == null)
+        {
+            throw new ArgumentNullException("Day la 1 chuoi null");
+        }
+        
+        string reverse = string.Empty;
+        for(int i = input.Length-1; i >= 0; i--)
+        {
+            reverse += input[i];    
+        }
+
+        return reverse;
+    }
+}
