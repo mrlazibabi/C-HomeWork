4ce0f5055f211ec7bab8a8bdd0f964e5254c833f
LeTuanAnh <leanh250103@gmail.com>
2025-02-11 21:46:18 +0700

    day13-bai2: loai bo ky tu trung lap

---
+++ C#HomeWork/loai bo ky tu trung lap/Program.cs
@@ -0,0 +1,41 @@
+﻿
+try
+{
+    DuplicateRemover remover = new DuplicateRemover();
+
+    string result1 = remover.RemoveDuplicates("helloo ol");
+    Console.WriteLine(result1); // 'helo '
+
+    string result2 = remover.RemoveDuplicates("programming");
+    Console.WriteLine(result2); // progamin
+
+    string result3 = remover.RemoveDuplicates("OpenAI");
+    Console.WriteLine(result3); // OpenAI
+
+    string result6 = remover.RemoveDuplicates(null); // ArgumentNullException 
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Message: {e.Message}");
+}    
+
+class DuplicateRemover
+{
+    public string RemoveDuplicates(string input)
+    {
+        if(input == null)
+        {
+            throw new ArgumentNullException("input must not be null");       
+        }
+
+        string newString = string.Empty;
+        for(int i = 0; i < input.Length; i++)
+        {
+            if(!newString.Contains(input[i]))
+            {
+                newString += input[i];
+            }
+        }
+        return newString;
+    }
+}
