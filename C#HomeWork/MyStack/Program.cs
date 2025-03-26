0c0f8106c8de6215aead1c21206779c916e7e385
LeTuanAnh <leanh250103@gmail.com>
2025-03-26 17:33:32 +0700

    day22-bai1: MyStack

---
+++ C#HomeWork/MyStack/Program.cs
@@ -0,0 +1,62 @@
+﻿MyStack demo = new MyStack(3);
+demo.Push(1);
+demo.Push(2);
+demo.Push(3);
+Console.WriteLine("Peek: " + demo.Peek());
+Console.WriteLine("Count: " + demo.Count);
+
+Console.WriteLine("Pop: " + demo.Pop());
+Console.WriteLine("Pop: " + demo.Pop());
+Console.WriteLine("Pop: " + demo.Pop());
+Console.WriteLine("Count: " + demo.Count);
+
+demo.Push(4);
+demo.Push(5);
+Console.WriteLine("Pop: " + demo.Pop());
+Console.WriteLine("Pop: " + demo.Pop());
+Console.WriteLine("Count: " + demo.Count);
+
+public class MyStack
+{
+    public int[] StackData;
+    public int Top;
+    public int MaxSize;
+
+    public MyStack(int maxSize)
+    {
+        MaxSize = maxSize;
+        StackData = new int[maxSize];
+        Top = -1;
+    }
+
+    public void Push(int item)
+    {
+        if (Top == MaxSize - 1)
+        {
+            throw new InvalidOperationException("Stack is full");
+        }
+        StackData[++Top] = item; ;
+    }
+
+    public int Pop()
+    {
+        if (Top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return StackData[Top--];
+    }
+
+    public int Peek()
+    {
+        if (Top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+        return StackData[Top];
+    }
+
+    public int Count => Top + 1;
+
+    public void Reset() => Top = -1;
+}
---
+++ C#HomeWork/MyStackTests/UnitTest1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Xunit;
+
+public class MyStackTests
+{
+    [Fact]
+    public void Push_ShouldAddElementToStack()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+
+        // Act
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        // Assert
+        Assert.Equal(3, stack.Count);
+        Assert.Equal(3, stack.Peek()); 
+    }
+
+    [Fact]
+    public void Push_ShouldThrowException_WhenStackIsFull()
+    {
+        // Arrange
+        MyStack stack = new MyStack(2);
+        stack.Push(1);
+        stack.Push(2);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => stack.Push(3));
+    }
+
+    [Fact]
+    public void Pop_ShouldRemoveAndReturnTopElement()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        // Act & Assert
+        Assert.Equal(3, stack.Pop());
+        Assert.Equal(2, stack.Pop());
+        Assert.Equal(1, stack.Pop());
+        Assert.Equal(0, stack.Count); 
+    }
+
+    [Fact]
+    public void Pop_ShouldThrowException_WhenStackIsEmpty()
+    {
+        // Arrange
+        MyStack stack = new MyStack(2);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
+    [Fact]
+    public void Peek_ShouldReturnTopElementWithoutRemovingIt()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+        stack.Push(1);
+        stack.Push(2);
+
+        // Act & Assert
+        Assert.Equal(2, stack.Peek()); 
+        Assert.Equal(2, stack.Count); 
+    }
+
+    [Fact]
+    public void Peek_ShouldThrowException_WhenStackIsEmpty()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => stack.Peek());
+    }
+
+    [Fact]
+    public void Count_ShouldReturnNumberOfElements()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+
+        // Act
+        stack.Push(1);
+        stack.Push(2);
+
+        // Assert
+        Assert.Equal(2, stack.Count);
+    }
+
+    [Fact]
+    public void Reset_ShouldClearStack()
+    {
+        // Arrange
+        MyStack stack = new MyStack(3);
+        stack.Push(1);
+        stack.Push(2);
+
+        // Act
+        stack.Reset();
+
+        // Assert
+        Assert.Equal(0, stack.Count);
+        Assert.Throws<InvalidOperationException>(() => stack.Peek()); 
+    }
+}
