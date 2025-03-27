5bb7dd343eba2df9a2c8db5e14c31b6c6b3249b5
LeTuanAnh <leanh250103@gmail.com>
2025-03-27 21:15:59 +0700

    day22-bai2: Queue

---
+++ C#HomeWork/MyQueue/Program.cs
@@ -0,0 +1,65 @@
+﻿MyQueue queue = new MyQueue(3);
+queue.Enqueue(10);
+queue.Enqueue(20);
+queue.Enqueue(30);
+
+Console.WriteLine(queue.Front());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Count);
+queue.Reset();
+Console.WriteLine(queue.Count);
+
+public class MyQueue
+{
+    public int[] Item;
+    public int Tail;
+    public int MaxQueue;
+
+    public MyQueue(int size)
+    {
+        MaxQueue = size;
+        Item = new int[MaxQueue];
+        Tail = -1;
+    }
+
+    public void Enqueue(int item)
+    {
+        if (Tail == MaxQueue - 1)
+        {
+            throw new InvalidOperationException("Queue is full");
+        }
+        Tail++;
+        Item[Tail] = item;
+    }
+
+    public int Dequeue()
+    {
+        if (Tail == -1)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        int frontItem = Item[0];
+
+        for(int i = 1; i <= Tail; i++)
+        {
+            Item[i - 1] = Item[i];
+        }
+
+        Tail--;
+        return frontItem;
+    }
+
+    public int Front()
+    {
+        if (Tail == -1)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        return Item[0];
+    }
+
+    public int Count => Tail + 1;
+
+    public void Reset() => Tail = -1;
+}
---
+++ C#HomeWork/MyQueueTests/UnitTest1.cs
@@ -0,0 +1,67 @@
+using System;
+using Xunit;
+
+public class MyQueueTests
+{
+    [Fact]
+    public void Enqueue_ShouldAddElementsToQueue()
+    {
+        var queue = new MyQueue(3);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+
+        Assert.Equal(10, queue.Front());
+        Assert.Equal(3, queue.Count);
+    }
+
+    [Fact]
+    public void Dequeue_ShouldRemoveAndReturnFrontElement()
+    {
+        var queue = new MyQueue(3);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+
+        Assert.Equal(10, queue.Dequeue());
+        Assert.Equal(20, queue.Front());
+        Assert.Equal(2, queue.Count);
+    }
+
+    [Fact]
+    public void Dequeue_ShouldThrowException_WhenQueueIsEmpty()
+    {
+        var queue = new MyQueue(3);
+
+        Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+    }
+
+    [Fact]
+    public void Enqueue_ShouldThrowException_WhenQueueIsFull()
+    {
+        var queue = new MyQueue(2);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+
+        Assert.Throws<InvalidOperationException>(() => queue.Enqueue(30));
+    }
+
+    [Fact]
+    public void Front_ShouldThrowException_WhenQueueIsEmpty()
+    {
+        var queue = new MyQueue(3);
+
+        Assert.Throws<InvalidOperationException>(() => queue.Front());
+    }
+
+    [Fact]
+    public void Reset_ShouldEmptyTheQueue()
+    {
+        var queue = new MyQueue(3);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Reset();
+
+        Assert.Equal(0, queue.Count);
+    }
+}
