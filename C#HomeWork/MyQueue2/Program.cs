8b98ae8ff72218e4292a00d1229b28b8c54f66b6
LeTuanAnh <leanh250103@gmail.com>
2025-04-04 00:05:02 +0700

    day23-bai4

---
+++ C#HomeWork/MyQueue2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+Queue queue = new Queue();
+public class Queue
+{
+    private LinkedList DataOfQueue;
+
+    public Queue()
+    {
+        DataOfQueue = new LinkedList();
+    }
+
+    public void Enqueue(int item)
+    {
+        DataOfQueue.InsertTail(new Node(item));
+    }
+
+    public int Dequeue()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue is empty.");
+
+        int value = DataOfQueue.GetHead().DataOfNode;
+        DataOfQueue.RemoveHead();
+        return value;
+    }
+
+    public int Front()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue is empty.");
+
+        return DataOfQueue.GetHead().DataOfNode;
+    }
+
+    public bool IsEmpty()
+    {
+        return DataOfQueue.IsEmpty();
+    }
+
+    public int Count => DataOfQueue.Count();
+}
