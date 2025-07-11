3cf6e680e3b796b59b5c725343f5b1fc039c06d1
LeTuanAnh <leanh250103@gmail.com>
2025-07-11 12:20:32 +0700

    #day37_update

--- C#HomeWork/TuDienAnhViet/Dictionary.cs
+++ C#HomeWork/TuDienAnhViet/Dictionary.cs
@@ -8,49 +8,54 @@ namespace TuDienAnhViet
 {
     public class Dictionary
     {
-        private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        private Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
         private string _dataPath;
 
         public Dictionary(string dataPath)
         {
-            dictionary = Utilities<Dictionary<string, string>>.ReadFile(dataPath);
+            dictionary = Utilities<Dictionary<string, List<string>>>.ReadFile(dataPath);
             _dataPath = dataPath;
         }
 
-        public void Add(string newWord, string meaningOfWord)
+        public void Add(string newWord, List<string> meaningOfWord)
         {
             if (dictionary.ContainsKey(newWord))
             {
                 throw new InvalidOperationException("New word is existed");
             }
             dictionary[newWord] = meaningOfWord;
-            Utilities<Dictionary<string, string>>.WriteFile(_dataPath, dictionary);
+            Utilities<Dictionary<string, List<string>>>.WriteFile(_dataPath, dictionary);
         }
 
-        public KeyValuePair<string, string> Search(string searchWord)
+        public KeyValuePair<string, List<string>> Search(string searchWord)
         {
-            foreach (KeyValuePair<string, string> keyValuePair in dictionary)
-            {
-                if (keyValuePair.Key.Equals(searchWord))
+            if (dictionary.ContainsKey(searchWord))
+                foreach (KeyValuePair<string, List<string>> keyValuePair in dictionary)
                 {
-                    return keyValuePair;
+                    if (keyValuePair.Key.Contains(searchWord))
+                    {
+                        return keyValuePair;
+                    }
                 }
-            }
+            KeyValuePair<string, List<string>> result = new KeyValuePair<string, List<string>>(searchWord, new List<string>());
+            result.Value.AddRange(dictionary.Keys.Where(k => dictionary[k].Contains(searchWord)));
+            if (result.Value.Count > 0)
+                return result;
             return default;
         }
 
         public void Remove(string removeWord)
         {
-            KeyValuePair<string, string> keyValuePairOfRemoveWord = Search(removeWord);
+            KeyValuePair<string, List<string>> keyValuePairOfRemoveWord = Search(removeWord);
             if (!keyValuePairOfRemoveWord.Equals(default))
             {
                 dictionary.Remove(removeWord);
-                Utilities<Dictionary<string, string>>.WriteFile(_dataPath, dictionary);
+                Utilities<Dictionary<string, List<string>>>.WriteFile(_dataPath, dictionary);
                 return;
             }
             throw new InvalidOperationException("Word not found");
         }
 
-        public Dictionary<string, string> GetDictionary() => dictionary;
+        public Dictionary<string, List<string>> GetDictionary() => dictionary;
     }
 }
--- C#HomeWork/TuDienAnhViet/DictionaryUI.cs
+++ C#HomeWork/TuDienAnhViet/DictionaryUI.cs
@@ -15,11 +15,26 @@ namespace TuDienAnhViet
         {
             Console.Write("Input newWord: ");
             string newWord = Console.ReadLine();
+            Console.Write("Input number of meaning: ");
+            if (!int.TryParse(Console.ReadLine(), out int numberOfMeaning))
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+            if (numberOfMeaning <= 0 || numberOfMeaning > 20)
+            {
+                Console.WriteLine("Invalid number of meaning");
+                return;
+            }
             Console.Write("Input meaning: ");
-            string meaning = Console.ReadLine();
+            List<string> listOfMeaning = new List<string>();
+            for (int i = 0; i < numberOfMeaning; i++)
+            {
+                listOfMeaning.Add(Console.ReadLine());
+            }
             try
             {
-                dictionary.Add(newWord, meaning);
+                dictionary.Add(newWord, listOfMeaning);
                 Console.WriteLine("Add success");
             }
             catch (InvalidOperationException ex)
@@ -33,13 +48,15 @@ namespace TuDienAnhViet
             Console.Write("Input searchWord: ");
             string searchWord = Console.ReadLine();
             var searchResult = dictionary.Search(searchWord);
-            if (searchResult.Equals(default(KeyValuePair<string, string>)))
+            if (searchResult.Equals(default(KeyValuePair<string, List<string>>)))
             {
                 Console.WriteLine("Not found");
             }
             else
             {
-                Console.WriteLine($"{searchResult.Key} | Meaning: {searchResult.Value}");
+                Console.WriteLine($"{searchResult.Key} \nMeaning or relevant word:");
+                foreach (var meaning in searchResult.Value) Console.Write($"{meaning} ");
+                Console.WriteLine();
             }
         }
 
@@ -61,10 +78,15 @@ namespace TuDienAnhViet
 
         public void GetDictionary()
         {
-            foreach (var word in dictionary.GetDictionary())
-            {
-                Console.WriteLine($"{word.Key} | Meaning: {word.Value}");
-            }
+            if (dictionary.GetDictionary().Count > 0)
+                foreach (var word in dictionary.GetDictionary())
+                {
+                    Console.WriteLine($"++{word.Key}++ \n--Meaning--:");
+                    foreach (var meaning in word.Value) Console.Write($"{meaning}.  ");
+                    Console.WriteLine("\n=========");
+                    Console.WriteLine();
+                }
+            else { Console.WriteLine("There is no word in dictionary"); }
         }
     }
 }
