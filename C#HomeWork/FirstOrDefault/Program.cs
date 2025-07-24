0435a41c7444f6a35178b9370805177e8ebb555e
LeTuanAnh <leanh250103@gmail.com>
2025-07-24 22:18:41 +0700

    day38_FirstOrDefault

---
+++ C#HomeWork/FirstOrDefault/Program.cs
@@ -0,0 +1,30 @@
+﻿List<int> items = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+NumberProcessor processor = new NumberProcessor();
+
+int a = processor.FirstOrDefault(items, item => item > 5);
+Console.WriteLine(a);
+
+int b = processor.FirstOrDefault(items, item => item / 3 == 3);
+Console.WriteLine(b);
+
+class NumberProcessor
+{
+    public int FirstOrDefault(List<int> items, Condition condition)
+    {
+        if (items == null || items.Count == 0)
+        {
+            throw new ArgumentNullException("items cannot be null or empty");
+        }
+
+        foreach (int item in items)
+        {
+            if (condition(item) == true)
+            {
+                return item;
+            }
+        }
+        return 0;
+    }
+}
+
+public delegate bool Condition(int item);
