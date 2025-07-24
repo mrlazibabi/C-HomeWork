8208464ad86f59c6e89056202f11100615769f57
LeTuanAnh <leanh250103@gmail.com>
2025-07-24 23:04:43 +0700

    day38_all

---
+++ C#HomeWork/DelegateAll/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> items = new List<int> { 2, 4, 6, 8 };
+List<int> items2 = new List<int> { 2, 4, 5, 8 };
+NumberProcessor processor = new NumberProcessor();
+
+bool a = processor.All(items, item => item % 2 == 0);
+Console.WriteLine(a);
+
+try
+{
+    bool b = processor.All(items2, item => item % 2 == 0);
+    Console.WriteLine(b);
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+class NumberProcessor
+{
+    public bool All(List<int> items, Condition condition)
+    {
+        if (items == null || items.Count == 0)
+        {
+            throw new ArgumentNullException("items cannot be null or empty");
+        }
+
+        foreach (int item in items)
+        {
+            if (!condition(item))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public delegate bool Condition(int item);
