78feba17566d6eebf053db94b42620278f252a8d
LeTuanAnh <leanh250103@gmail.com>
2025-08-17 15:59:12 +0700

    day40_GenExSkip

---
+++ C#HomeWork/GenExSkip/Program.cs
@@ -0,0 +1,34 @@
+﻿using Dumpify;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+var skipped = numbers.Skip(2);
+skipped.Dump(); 
+
+List<int> numbers2 = new List<int> { 1, 2 };
+var skipped2 = numbers2.Skip(5);
+skipped2.Dump();
+
+public static class ListExtensions
+{
+    public static List<T> Skip<T>(List<T> items, int count)
+    {
+        List<T> result = new List<T>();
+
+        int skippedCount = 0;
+
+        foreach (T item in items)
+        {
+            if (skippedCount < count)
+            {
+                skippedCount++;
+                continue;
+            }
+
+            if (skippedCount == count && items != null)
+            {
+                result.Add(item);
+            }
+        }
+        return result;
+    }
+}
