97dfc4d9237a70d1b43b0bd64db9e67afe6cfdb9
LeTuanAnh <leanh250103@gmail.com>
2025-08-17 13:17:08 +0700

    day40_ExFirst

---
+++ C#HomeWork/ExtensionMethodFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dumpify;
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+int firstEven = numbers.First(n => n % 2 == 0);
+firstEven.Dump("First number to be an Even Number: ");
+
+public static class FirstExtension
+{
+    public static int First(this IEnumerable<int> items, Predicate<int> predicate)
+    {
+        if (items == null) throw new ArgumentNullException(nameof(items));
+        if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+        foreach (int item in items)
+        {
+            if (predicate(item))
+            {
+                return item;
+            }
+        }
+        throw new InvalidOperationException("No element satisfies the condition in predicate.");
+    }
+}
