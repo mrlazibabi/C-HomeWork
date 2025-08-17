943eb5c88499546f774b809b55194e8804776290
LeTuanAnh <leanh250103@gmail.com>
2025-08-17 14:49:35 +0700

    day40_GenExFirst

---
+++ C#HomeWork/GenExFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+int firstNumber = numbers.First();
+
+public static class ListExtensions
+{
+    public static T First<T>(this List<T> list)
+    {
+        if (list == null || list.Count == 0)
+        {
+            throw new InvalidOperationException("The list is empty or null.");
+        }
+        return list[0];
+    }
+}
