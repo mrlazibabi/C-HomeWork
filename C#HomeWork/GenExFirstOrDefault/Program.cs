10e4a9ae1faf79ae8d753d8f373ecc232cb13e83
LeTuanAnh <leanh250103@gmail.com>
2025-08-17 14:57:58 +0700

    day40_GenExFirstOrDefault

---
+++ C#HomeWork/GenExFirstOrDefault/Program.cs
@@ -0,0 +1,31 @@
+﻿
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+int firstNumber = numbers.FirstOrDefault(number => number > 4);
+Console.WriteLine(firstNumber);
+
+int firstNumber2 = numbers.FirstOrDefault(number => number > 5);
+Console.WriteLine(firstNumber2);
+
+public static class ListExtensions
+{
+    public static T FirstOrDefault<T>(this List<T> list, Predicate<T> predicate)
+    {
+        if (list == null || list.Count == 0)
+        {
+            throw new InvalidOperationException("The list is empty or null.");
+        }
+        if (predicate == null)
+        {
+            throw new ArgumentNullException(nameof(predicate), "Predicate cannot be null.");
+        }
+
+        foreach (var item in list)
+        {
+            if (predicate(item))
+            {
+                return item;
+            }
+        }
+        return default(T);
+    }
+}
