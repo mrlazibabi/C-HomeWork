0fb31cc99eba29efeb5e80d18996572a49a0dec6
LeTuanAnh <leanh250103@gmail.com>
2025-08-21 00:11:37 +0700

    day44-Hash-Salt

---
+++ C#HomeWork/Hash&Salt/Program.cs
@@ -0,0 +1,52 @@
+﻿Authen authen = new Authen();
+bool isRunning = true;
+
+while (isRunning)
+{
+    Console.WriteLine("Welcome to the Authentication System!");
+    Console.WriteLine("1. Register");
+    Console.WriteLine("2. Login");
+    Console.WriteLine("3. Exit");
+    Console.Write("Please select an option: ");
+    
+    string choice = Console.ReadLine();
+    
+    switch (choice)
+    {
+        case "1":
+            Console.Write("Enter username: ");
+            string regUsername = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string regPassword = Console.ReadLine();
+            if (authen.Register(regUsername, regPassword))
+            {
+                Console.WriteLine("Registration successful!");
+            }
+            else
+            {
+                Console.WriteLine("Username already exists.");
+            }
+            break;
+        case "2":
+            Console.Write("Enter username: ");
+            string loginUsername = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string loginPassword = Console.ReadLine();
+            if (authen.Login(loginUsername, loginPassword))
+            {
+                Console.WriteLine("Login successful!");
+                return; // Exit the loop on successful login
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password.");
+            }
+            break;
+        case "3":
+            isRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+}
