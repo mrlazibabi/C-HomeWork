82b8ec69021436477b41ae32db594a9a24863e40
LeTuanAnh <leanh250103@gmail.com>
2025-08-24 17:12:16 +0700

    day45_PhoneNumber

---
+++ C#HomeWork/PhoneNumberRegex/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dumpify;
+using System.Text.RegularExpressions;
+
+Regex regex = new Regex(@"^\+[1-9]\d{1,11}$");
+string[] testNumbers = new[]
+{
+    "+1234567890",    // Valid
+    "+19876543210",   // Valid
+    "+123",           // Invalid (too short)
+    "1234567890",     // Invalid (missing '+')
+    "+123456789012345" // Invalid (too long)
+};
+
+MatchCollection[] results = testNumbers
+    .Select(number => regex.Matches(number))
+    .ToArray();
+
+foreach (MatchCollection result in results)
+{
+    result.Dump();
+}
